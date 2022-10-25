f6ecc1c21731e37a33ed52bad387c40eec180c1f
unknown <alexis.0529@hotmail.com>
2022-10-25 05:07:21 -0600

    Examen terminado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { AuthGuardService } from './guards/auth.service';
+import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
+import { EditorLayoutComponent } from './layout/editor-layout/editor-layout.component';
+import { ViewerLayoutComponent } from './layout/viewer-layout/viewer-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "login", component:LoginComponent },
+  { path: "register", component:RegisterComponent },
+  { path: "userList", component:UserListComponent, canActivate: [AuthGuardService] },
+  { path: "adminLayout", component:AdminLayoutComponent },
+  { path: "editorLayout", component:EditorLayoutComponent },
+  { path: "viewerLayout", component:ViewerLayoutComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { BlockUiTemplateComponent } from './sharedModule/block-ui-template/block-ui-template.component';
+import { BlockUIModule } from 'ng-block-ui';
+import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
+import { EditorLayoutComponent } from './layout/editor-layout/editor-layout.component';
+import { ViewerLayoutComponent } from './layout/viewer-layout/viewer-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    UserListComponent,
+    BlockUiTemplateComponent,
+    AdminLayoutComponent,
+    EditorLayoutComponent,
+    ViewerLayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    BlockUIModule.forRoot({
+      template: BlockUiTemplateComponent
+  }),
   ],
   providers: [],
   bootstrap: [AppComponent]
